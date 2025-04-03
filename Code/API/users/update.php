bc095465f9bad06737d007b17f0bdf54fd019c6d
Louisann-Borg <93986705+CrimsonQween@users.noreply.github.com>
2025-04-03 19:25:49 +0200

    Added Update User Endpoint
    Created the update user endpoint which functions but has a small error that needs fixing

---
+++ Code/API/users/update.php
@@ -0,0 +1,27 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json");
+header("Access-Control-Allow-Methods: PUT");
+header("Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With");
+
+include_once("../../core/initialize.php");
+
+$users = new Users($db);
+
+$data = json_decode(file_get_contents("php://input"));
+
+$users->id = $data->id;
+$users->username = $data->username;
+$users->email = $data->email;
+$users->password = $data->password;
+$users->firstName = $data->firstName;
+$users->lastName = $data->lastName;
+
+if($users->update()){
+    echo json_encode(array('message'=> 'User Updated'));
+}
+else{
+    echo json_encode(array('message'=> 'User NOT Updated'));
+}
+
+?>
--- Code/Includes/users.php
+++ Code/Includes/users.php
@@ -17,34 +17,59 @@ class Users {
         $this->conn = $db;
     }
 
-// Create User
+// Create User Endpoint
 public function create() {
     $query = "INSERT INTO users (username, email, password, firstName, lastName) 
             VALUES (:username, :email, :password, :first_name, :last_name)";
     
     $stmt = $this->conn->prepare($query);
 
-    // Bind values
     $stmt->bindParam(":username", $this->username);
     $stmt->bindParam(":email", $this->email);
     $stmt->bindParam(":password", $this->password);
     $stmt->bindParam(":first_name", $this->firstName);
     $stmt->bindParam(":last_name", $this->lastName);
 
-    // Execute query
     return $stmt->execute();
 }
 
-//  Get Users
+//  Get Users Endpoint
 public function read($limit = 10, $offset = 0) {
     $query = "SELECT id, username, email, firstName, lastName FROM " . $this->table . " LIMIT :limit OFFSET :offset";
     $stmt = $this->conn->prepare($query);
     
-    // Bind limit and offset
     $stmt->bindParam(":limit", $limit, PDO::PARAM_INT);
     $stmt->bindParam(":offset", $offset, PDO::PARAM_INT);
     $stmt->execute();
     
     return $stmt;
 }
+
+// Update User Endpoint#
+public function update(){
+    $query = "UPDATE {$this->table} SET username =:username, email = :email , password = :password ,firstName = :firstName , lastName = :lastName WHERE id = :id;";
+
+    $stmt = $this->conn->prepare($query);
+    
+    $this->id = htmlspecialchars(strip_tags($this->id));
+    $this->username = htmlspecialchars(strip_tags($this->username));
+    $this->email = htmlspecialchars(strip_tags($this->email));
+    $this->password = htmlspecialchars(strip_tags($this->password));
+    $this->firstName = htmlspecialchars(strip_tags($this->firstName));
+    $this->lastName = htmlspecialchars(strip_tags($this->lastName));
+
+    $stmt->bindParam(":id", $this->id);
+    $stmt->bindParam(":username", $this->username);
+    $stmt->bindParam(":email", $this->email);
+    $stmt->bindParam(":password", $this->password);
+    $stmt->bindParam(":firstName", $this->firstName);
+    $stmt->bindParam(":lastName", $this->lastName);
+
+    if($stmt->execute()){
+        return true;
+    }
+    printf("Error %s. \n", $stmt->error);
+    return false;
+}
+
 }
